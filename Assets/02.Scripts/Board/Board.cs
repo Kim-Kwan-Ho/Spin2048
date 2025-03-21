3e31a96796c7e4a4ab8bea6de983b716131ea006
KwanHo <70732356+Kim-Kwan-Ho@users.noreply.github.com>
2025-03-21 17:09:46 +0900

    Develop (#27)
    * [feat] 2024 board
    
    - 기본적인 2024게임 제작
    
    * [feat] BoardRotation
    
    - Tile과 함께 Board 회전 구현
    
    * [fix] Board Rotation (#3)
    
    * [fix] Board Rotation
    
    - 회전 시 딜레이 없이 작동하는 문제 수정
    - 회전 시 보드 합성이 누락되는 문제 수정
    
    * Update BoardSettings.asset
    
    * [feat] GameOver (#4)
    
    * [feat] Restart (#5)
    
    - 보드 초기화 기능 제작
    - 셀의 초기화가 제대로 되지 않았던 오류 수정
    
    * [feat] buttons (#6)
    
    - 좌,우측 회전 버튼 연동
    - 생성 버튼 연동
    
    * [feat] GameOver&Restart
    
    - 게임 패배
    - 게임 재시작
    
    * [feat] Score
    
    - 스코어 설정
    - 최고기록 저장
    - UI 연동
    
    * [feat] GameOver&Restart (#7)
    
    - 게임 패배
    - 게임 재시작
    
    * [update] git ignore
    
    - 다운 받은 에셋들 올라가지 않도록 설정
    
    * [update] gitignore
    
    * [feat] Resolution (#9)
    
    - 해상도에 따라 카메라 orthographicSize 조정
    
    * [feat] Board background (#16)
    
    - Cell 간격 설정
    - Cell 백그라운드 제작
    
    * [design] buttons & bg (#22)
    
    * [feat] Board background
    
    - Cell 간격 설정
    - Cell 백그라운드 제작
    
    * [design] buttons
    
    * [design] score & retry & gameover (#23)
    
    * [update] board settings (#24)
    
    * [fix] build fix (#25)
    
    - 빌드 오류 수정

---
+++ Assets/02.Scripts/Board/Board.cs
@@ -0,0 +1,312 @@
+using System.Collections;
+using System.Collections.Generic;
+using KKH.Manager;
+using UnityEngine;
+
+
+
+namespace KKH.Board
+{
+    public class Board : BaseBehaviour
+    {
+        [Header("Board Setting")]
+        [SerializeField] private BoardSettingSo _boardSetting;
+        private BoardRotation _boardRotation;
+       [SerializeField]  private GameObject _boardBackground;
+
+        [Header("Cells")]
+        [SerializeField] private GameObject _cellPrefab;
+        private Cell[,] _cells;
+
+
+        [Header("Tiles")]
+        [SerializeField] private GameObject _tilePrefab;
+        private List<Tile> _tileList;
+
+
+        private bool _canMove;
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+
+            _boardRotation = new BoardRotation(_boardSetting.Col, _boardSetting.Row);
+            _cells = new Cell[_boardSetting.Col, _boardSetting.Row];
+            _tileList = new List<Tile>(_boardSetting.GetTotalTileCount());
+
+
+            CreateNewBoard();
+        }
+
+
+        private void OnEnable()
+        {
+            GameManager.Instance.OnGameRestart += ResetBoard;
+        }
+
+        private void OnDisable()
+        {
+            GameManager.Instance.OnGameRestart -= ResetBoard;
+        }
+        private void CreateNewBoard()
+        {
+            CreateBoardBackground();
+            _tileList.Clear();
+            float boardWidth = _boardSetting.Col + (_boardSetting.Col - 1) * _boardSetting.CellOffSet;
+            float boardHeight = _boardSetting.Row + (_boardSetting.Row - 1) * _boardSetting.CellOffSet;
+
+            float xStart = -boardWidth / 2 + 0.5f;
+            float yStart = -boardHeight / 2 + 0.5f;
+
+            float offSetY = 0;
+
+            for (int r = 0; r < _boardSetting.Row; r++)
+            {
+                float offSetX = 0;
+
+                for (int c = 0; c < _boardSetting.Col; c++)
+                {
+                    _cells[r, c] = Instantiate(
+                        _cellPrefab,
+                        new Vector2(xStart + c + offSetX, yStart + r + offSetY),
+                        Quaternion.identity
+                    ).GetComponent<Cell>();
+
+                    _cells[r, c].SetCell(new Vector2Int(r, c));
+                    offSetX += _boardSetting.CellOffSet;
+                }
+
+                offSetY += _boardSetting.CellOffSet;
+            }
+
+            _canMove = true;
+            SpawnTile();
+        }
+        private void CreateBoardBackground()
+        {
+            GameObject boardBackground = Instantiate(_boardBackground);
+
+            float boardWidth = _boardSetting.Col + (_boardSetting.Col - 1) * _boardSetting.CellOffSet + _boardSetting.BackGroundPadding * 2;
+            float boardHeight = _boardSetting.Row + (_boardSetting.Row - 1) * _boardSetting.CellOffSet + _boardSetting.BackGroundPadding * 2;
+
+            boardBackground.transform.position = new Vector2(0,0);
+            boardBackground.transform.localScale = new Vector3(boardWidth, boardHeight, 1);
+        }
+        private void ResetBoard()
+        {
+            foreach (var tile in _tileList)
+            {
+                tile.DestroyTile();
+            }
+            _tileList.Clear();
+
+            _canMove = true;
+            SpawnTile();
+        }
+        public void SpawnTile()
+        {
+            if (!_canMove)
+                return;
+
+            Cell cell = GetRandomEmptyCell();
+
+            if (cell == null)
+            {
+                return;
+            }
+            else
+            {
+                Tile tile = Instantiate(_tilePrefab, transform).GetComponent<Tile>();
+                tile.SpawnTile(cell, _boardSetting.GetSpawnTileLevel());
+                _tileList.Add(tile);
+
+                if (_tileList.Count >= _boardSetting.GetTotalTileCount() && CheckGameOver())
+                {
+                    _canMove = false;
+                    GameManager.Instance.OnGameOver?.Invoke();
+                }
+            }
+        }
+
+        public void TurnBoard(bool left)
+        {
+            if (!_canMove)
+                return;
+
+            Dictionary<Tile, Vector2Int> newPositions = new Dictionary<Tile, Vector2Int>();
+
+            foreach (var tile in _tileList)
+            {
+                var pos = left ? _boardRotation.RightRotationDic[tile.Cell.Coordinates] : _boardRotation.LeftRotationDic[tile.Cell.Coordinates];
+                newPositions[tile] = pos;
+                tile.ChangeCell(null);
+            }
+
+            foreach (var tile in _tileList)
+            {
+                var newPos = newPositions[tile];
+                tile.Move(_cells[newPos.x, newPos.y]);
+            }
+            StartCoroutine(CoPullDownBoardDelay());
+        }
+
+        private IEnumerator CoPullDownBoardDelay()
+        {
+            _canMove = false;
+            yield return new WaitForSeconds(_boardSetting.RotateTime);
+            PullDownTiles();
+        }
+        private void PullDownTiles()
+        {
+            Move(Vector2Int.left, 1, 1, 0, 1);
+        }
+
+
+
+        private Cell GetRandomEmptyCell()
+        {
+            HashSet<Vector2Int> hash = new HashSet<Vector2Int>();
+            while (true)
+            {
+                int x = Random.Range(0, _boardSetting.Row);
+                int y = Random.Range(0, _boardSetting.Col);
+
+                if (hash.Contains(new Vector2Int(x, y)))
+                    continue;
+
+                if (_cells[x, y].Tile == null)
+                    return _cells[x, y];
+
+                hash.Add(new Vector2Int(x, y));
+                if (hash.Count >= _boardSetting.GetTotalTileCount())
+                {
+                    return null;
+                }
+            }
+        }
+
+        private void Move(Vector2Int direction, int startX, int incrementX, int startY, int incrementY)
+        {
+
+            int score = 0;
+            for (int x = startX; x >= 0 && x < _boardSetting.Row; x += incrementX)
+            {
+                for (int y = startY; y >= 0 && y < _boardSetting.Col; y += incrementY)
+                {
+                    Cell cell = _cells[x, y];
+                    if (cell.Tile != null)
+                    {
+                        score += MoveTile(cell.Tile, direction);
+                    }
+                }
+            }
+
+            if (score != 0)
+            {
+                ScoreManager.Instance.AddScore(score);
+            }
+            StartCoroutine(CoWaitForChanges());
+        }
+
+
+        private int MoveTile(Tile tile, Vector2Int direction)
+        {
+            Cell newCell = null;
+            Cell adjacent = GetNextCell(tile.Cell, direction);
+
+            while (adjacent != null)
+            {
+                if (adjacent.Tile != null)
+                {
+                    if (tile.CheckMerge(adjacent.Tile))
+                    {
+
+                        return MergeTiles(tile, adjacent.Tile);
+                    }
+                    break;
+                }
+
+                newCell = adjacent;
+                adjacent = GetNextCell(adjacent, direction);
+            }
+
+            if (newCell != null)
+            {
+                tile.Move(newCell);
+            }
+
+            return 0;
+        }
+        private int MergeTiles(Tile a, Tile b)
+        {
+            _tileList.Remove(a);
+            a.Merge(b.Cell);
+            return b.UpgradeTileStep();
+        }
+
+        private IEnumerator CoWaitForChanges()
+        {
+            yield return new WaitForSeconds(_boardSetting.DelayTime);
+            foreach (var tile in _tileList)
+            {
+                tile.UnLockTile();
+            }
+
+            yield return new WaitForSeconds(_boardSetting.SpawnDelayTime);
+            if (_tileList.Count >= _boardSetting.GetTotalTileCount())
+            {
+                GameManager.Instance.OnGameOver?.Invoke();
+            }
+            else
+            {
+                _canMove = true;
+                SpawnTile();
+            }
+        }
+        private Cell GetNextCell(Cell cell, Vector2Int direction)
+        {
+            return GetCell(cell.Coordinates.x + direction.x, cell.Coordinates.y - direction.y);
+        }
+        private Cell GetCell(int x, int y)
+        {
+            return (x >= 0 && x < _boardSetting.Row && y >= 0 && y < _boardSetting.Col) ? _cells[x, y] : null;
+        }
+
+
+
+
+        private bool CheckGameOver()
+        {
+            Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+            foreach (var tile in _tileList)
+            {
+                foreach (var direction in directions)
+                {
+                    Cell adjacentCell = GetNextCell(tile.Cell, direction);
+                    if (adjacentCell != null && tile.CheckMerge(adjacentCell.Tile))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+
+#if UNITY_EDITOR
+        protected override void OnBindField()
+        {
+            base.OnBindField();
+            _boardSetting = FindObjectInAsset<BoardSettingSo>();
+            _boardBackground = FindObjectInAsset<GameObject>("BoardBackground", EDataType.prefab);
+            _cellPrefab = FindObjectInAsset<Cell>().gameObject;
+            _tilePrefab = FindObjectInAsset<Tile>().gameObject;
+        }
+
+        protected override void OnButtonField()
+        {
+            SpawnTile();
+        }
+#endif
+    }
+}
